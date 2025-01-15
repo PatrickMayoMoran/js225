b47484c0f378c56eefb0c2261c73f7b08d9663db
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-14 20:51:37 -0800

    Update smartphone constructor to include two methods

--- book/typesAndObjects/4.js
+++ book/typesAndObjects/4.js
@@ -2,10 +2,18 @@ function  SmartPhone(brand, model, releaseYear) {
   this.brand = brand;
   this.model = model;
   this.releaseYear = releaseYear;
+
+  this.checkBatteryLevel = function() {
+    console.log("Checking battery power...");
+  }
+
+  this.displayInfo = function() {
+    console.log(`This is a ${this.model} made by ${this.brand} from ${this.releaseYear}`);
+  }
 }
 
 let iPhone = new SmartPhone("Apple", "iPhone 12", 2020);
 let galaxy = new SmartPhone("Samsung", "Galaxy S21", 2021);
 
-console.log(iPhone.releaseYear);
-console.log(galaxy.releaseYear);
+iPhone.displayInfo();
+galaxy.displayInfo();
