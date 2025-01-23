8d23604fb9f8758702df4e1152395337f92fd6aa
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-23 13:51:13 -0800

    Add solution to partial function application problem that creates a new function with a set function and argument

---
+++ lesson4/makePartialFunc.js
@@ -0,0 +1,20 @@
+function makePartialFunc(func, b) {
+  // implement this function...
+  return function(a) {
+    return func(a, b);
+  };
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+let multiplyBy5 = makePartialFunc(multiply, 5);
+let divideBy2 = makePartialFunc(divide, 2);
+
+console.log(multiplyBy5(100)); // 500
+console.log(divideBy2(100)); // 50
