3660e5324bc7675ffc29dc9553659ff3aaa30e69
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-23 10:17:24 -0800

    Convert account object to be returned by a makeAccount function

--- lesson4/bankingProject.js
+++ lesson4/bankingProject.js
@@ -1,24 +1,28 @@
-let account = {
-  balance: 0,
+function makeAccount() {
+  return {
+    balance: 0,
 
-  transactions: [],
+    transactions: [],
 
-  deposit(amount) {
-    this.balance += amount;
-    this.transactions.push({type: "deposit", amount: amount});
-    return amount;
-  },
+    deposit(amount) {
+      this.balance += amount;
+      this.transactions.push({type: "deposit", amount: amount});
+      return amount;
+    },
 
-  withdraw(amount) {
-    if (amount > this.balance) {
-      amount = this.balance;
+    withdraw(amount) {
+      if (amount > this.balance) {
+        amount = this.balance;
+      }
+
+      this.balance -= amount;
+      this.transactions.push({type: "withdraw", amount: amount});
+      return amount;
     }
-
-    this.balance -= amount;
-    this.transactions.push({type: "withdraw", amount: amount});
-    return amount;
-  },
+  };
 }
+
+let account = makeAccount();
 
 console.log(account.deposit(100));
 console.log(account.transactions);
@@ -28,3 +32,5 @@ console.log(account.withdraw(91));
 console.log(account.transactions);
 console.log(account.balance);
 
+let otherAccount = makeAccount();
+console.log(otherAccount.balance)
