2f48c70b6fd9c2f47dae8af4abf3259db254bead
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-23 10:11:13 -0800

    Add own solution to withdraw method that adjusts for attempted overdrafts

--- lesson4/bankingProject.js
+++ lesson4/bankingProject.js
@@ -5,11 +5,22 @@ let account = {
     this.balance += amount;
     return amount;
   },
+
+  withdraw(amount) {
+    if (amount <= this.balance) {
+      this.balance -= amount;
+      return amount;
+    } else {
+      let existingBalance = this.balance;
+      this.balance = 0;
+      return existingBalance;
+    }
+  }
 }
 
+console.log(account.deposit(100));
+console.log(account.withdraw(19));
 console.log(account.balance);
-console.log(account.deposit(12));
-console.log(account.balance);
-console.log(account.deposit(10));
+console.log(account.withdraw(91));
 console.log(account.balance);
 
