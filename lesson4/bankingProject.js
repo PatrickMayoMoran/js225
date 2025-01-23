fec5802a628ccbd867ba11e9498b67c47f715901
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-23 10:08:25 -0800

    Add simple deposit method to account object

--- lesson4/bankingProject.js
+++ lesson4/bankingProject.js
@@ -1,3 +1,15 @@
 let account = {
-  balance: 0;
+  balance: 0,
+
+  deposit(amount) {
+    this.balance += amount;
+    return amount;
+  },
 }
+
+console.log(account.balance);
+console.log(account.deposit(12));
+console.log(account.balance);
+console.log(account.deposit(10));
+console.log(account.balance);
+
