8deecbef7073f28cef6401ffac7e1f99a860c4eb
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-23 14:18:38 -0800

    Use immediately invoked function expression to create a private scope for accessing and updating a unique student id

---
+++ lesson4/generateStudentId.js
@@ -0,0 +1,12 @@
+let generateStudentId = (function() {
+  let studentId = 0;
+
+  return function() {
+    studentId += 1;
+    return studentId;
+  };
+})();
+
+console.log(generateStudentId());
+console.log(generateStudentId());
+console.log(generateStudentId());
