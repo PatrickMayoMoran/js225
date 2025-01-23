0ac66a9fa180e008e0c526e96cfd2af58e51594c
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-23 14:55:12 -0800

    Add provided solution for implementing countdown with an immediately invoked function expression in the larger function definition

--- lesson4/countdown.js
+++ lesson4/countdown.js
@@ -1,11 +1,11 @@
-let countdown = (function() {
-  return function(num) {
+function countdown(count) {
+  (function(num) {
     let i;
     for (i = num; i >= 0; i--) {
       console.log(i);
     }
     console.log('Done!');
-  };
-})();
+  })(count);
+}
 
 countdown(7);
