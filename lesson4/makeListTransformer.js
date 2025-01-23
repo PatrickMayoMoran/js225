2a97cfa315d2780a1658013573b20212d42e5f60
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-22 18:47:16 -0800

    Add solution to make List transformer practice problem

---
+++ lesson4/makeListTransformer.js
@@ -0,0 +1,12 @@
+function makeListTransformer(func) {
+  // ... implement this function
+  return function(list) {
+    return list.map(func);
+  }
+}
+
+let timesTwo = makeListTransformer(function(number) {
+  return number * 2;
+});
+
+console.log(timesTwo([1, 2, 3, 4])); // [2, 4, 6, 8]
