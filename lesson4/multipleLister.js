80b4e48ba754af4b32430711dbd0c8b23ddeec3d
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-22 19:10:37 -0800

    Add solution to multiple Lister higher order function practice problem

---
+++ lesson4/multipleLister.js
@@ -0,0 +1,19 @@
+function makeMultipleLister(number) {
+  return function() {
+    for (let multiple = number; multiple < 100; multiple += number) {
+      console.log(multiple);
+    }
+  }
+}
+
+let lister = makeMultipleLister(13);
+lister();
+/*
+13
+26
+39
+52
+65
+78
+91
+*/
