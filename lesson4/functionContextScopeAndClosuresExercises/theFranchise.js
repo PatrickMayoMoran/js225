602b6d359468fee767fa838a970a88e7f82e96a1
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-23 20:01:35 -0800

    Add arrow function solution to the franchise problem

--- lesson4/functionContextScopeAndClosuresExercises/theFranchise.js
+++ lesson4/functionContextScopeAndClosuresExercises/theFranchise.js
@@ -1,14 +1,24 @@
 const franchise = {
   name: 'How to Train Your Dragon',
   allMovies() {
+    return [1, 2, 3].map(n => {
+      return `${this.name} ${n}`;
+    });
+  },
+}
+/* SELF - Self to reference this
+const franchise = {
+  name: 'How to Train Your Dragon',
+  allMovies() {
     let self = this;
     return [1, 2, 3].map(function(number) {
       return `${self.name} ${number}`;
     });
   },
 }
+*/
 
-/* IIFE ACCESS
+/* IIFE - using immediately invoked function expression with argument
 const franchise = {
   name: 'How to Train Your Dragon',
   allMovies() {
