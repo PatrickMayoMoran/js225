54cfd8fb7757088dba4d3914653ccd9a8509c87f
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-23 20:04:04 -0800

    Add this optional argument to array functions solution to the franchise problem

--- lesson4/functionContextScopeAndClosuresExercises/theFranchise.js
+++ lesson4/functionContextScopeAndClosuresExercises/theFranchise.js
@@ -1,11 +1,23 @@
 const franchise = {
   name: 'How to Train Your Dragon',
   allMovies() {
+    return [1, 2, 3].map(function(number) {
+      return `${this.name} ${number}`;
+    }, this);
+  },
+};
+
+/* ARROW - Arrow function which retains this of space where defined
+const franchise = {
+  name: 'How to Train Your Dragon',
+  allMovies() {
     return [1, 2, 3].map(n => {
       return `${this.name} ${n}`;
     });
   },
 }
+*/
+
 /* SELF - Self to reference this
 const franchise = {
   name: 'How to Train Your Dragon',
