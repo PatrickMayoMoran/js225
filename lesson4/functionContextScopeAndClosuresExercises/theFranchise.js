7bcb239be9beb07224be995e9f4b2a64e0469397
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-23 17:42:02 -0800

    Add self reference to this solution for theFranchise problem

--- lesson4/functionContextScopeAndClosuresExercises/theFranchise.js
+++ lesson4/functionContextScopeAndClosuresExercises/theFranchise.js
@@ -1,6 +1,17 @@
 const franchise = {
   name: 'How to Train Your Dragon',
   allMovies() {
+    let self = this;
+    return [1, 2, 3].map(function(number) {
+      return `${self.name} ${number}`;
+    });
+  },
+}
+
+/* IIFE ACCESS
+const franchise = {
+  name: 'How to Train Your Dragon',
+  allMovies() {
     return (function(name) {
       return [1, 2, 3].map(function(number) {
         return `${name} ${number}`;
@@ -8,5 +19,6 @@ const franchise = {
   })(this.name);
   }
 };
+*/
 
 console.log(franchise.allMovies());
