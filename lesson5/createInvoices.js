d756b022f4a918d73798c0b6546570f336c44cf2
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-25 14:49:15 -0800

    Add prompt for creating payment object factory

--- lesson5/createInvoices.js
+++ lesson5/createInvoices.js
@@ -21,19 +21,54 @@ function invoiceTotal(invoices) {
   return total;
 }
 
-let invoices = [];
-invoices.push(createInvoice());
-invoices.push(createInvoice({
+// let invoices = [];
+// invoices.push(createInvoice());
+// invoices.push(createInvoice({
+//   internet: 6500,
+// }));
+// 
+// invoices.push(createInvoice({
+//   phone: 2000,
+// }));
+// 
+// invoices.push(createInvoice({
+//   phone: 1000,
+//   internet: 4500,
+// }));
+// 
+// console.log(invoiceTotal(invoices));             // => 31000
+function createPayment(services) {
+  // implement the factory function here
+}
+
+function paymentTotal(payments) {
+  let total = 0;
+  let i;
+
+  for (i = 0; i < payments.length; i += 1) {
+    total += payments[i].total();
+  }
+
+  return total;
+}
+
+let payments = [];
+payments.push(createPayment());
+payments.push(createPayment({
   internet: 6500,
 }));
 
-invoices.push(createInvoice({
+payments.push(createPayment({
   phone: 2000,
 }));
 
-invoices.push(createInvoice({
+payments.push(createPayment({
   phone: 1000,
   internet: 4500,
 }));
 
-console.log(invoiceTotal(invoices));             // => 31000
+payments.push(createPayment({
+  amount: 10000,
+}));
+
+console.log(paymentTotal(payments));      // => 24000
