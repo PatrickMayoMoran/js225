9d631e1a4248e480d8a8539e73c2b59e8b4f8ac6
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-25 15:55:42 -0800

    ls

--- lesson5/createInvoices.js
+++ lesson5/createInvoices.js
@@ -1,12 +1,28 @@
 function createInvoice(services={}) {
   // implement the factory function here
+  let paymentsReceived = [];
   return {
     internet: services.internet || 5500,
     phone: services.phone || 3000,
 
     total() {
       return this.internet + this.phone;
-    }
+    },
+
+    addPayment(payment) {
+      paymentsReceived.push(payment);
+    },
+
+    addPayments(payments) {
+      paymentsReceived.push(...payments);
+    },
+
+    amountDue() {
+      let paid =  paymentsReceived.reduce((total, payment) => {
+        return total + payment.total();
+      }, 0);
+      return this.total() - paid;
+    },
   }
 }
 
@@ -65,23 +81,44 @@ function paymentTotal(payments) {
   return total;
 }
 
-let payments = [];
-payments.push(createPayment());
-payments.push(createPayment({
-  internet: 6500,
-}));
+// let payments = [];
+// payments.push(createPayment());
+// payments.push(createPayment({
+//   internet: 6500,
+// }));
+// 
+// payments.push(createPayment({
+//   phone: 2000,
+// }));
+// 
+// payments.push(createPayment({
+//   phone: 1000,
+//   internet: 4500,
+// }));
+// 
+// payments.push(createPayment({
+//   amount: 10000,
+// }));
+// 
+// console.log(paymentTotal(payments));      // => 24000
+let invoice = createInvoice({
+  phone: 1200,
+  internet: 4000,
+});
 
-payments.push(createPayment({
-  phone: 2000,
-}));
+let payment1 = createPayment({
+  amount: 2000,
+});
 
-payments.push(createPayment({
+let payment2 = createPayment({
   phone: 1000,
-  internet: 4500,
-}));
+  internet: 1200,
+});
 
-payments.push(createPayment({
-  amount: 10000,
-}));
+let payment3 = createPayment({
+  phone: 1000,
+});
 
-console.log(paymentTotal(payments));      // => 24000
+invoice.addPayment(payment1);
+invoice.addPayments([payment2, payment3]);
+console.log(invoice.amountDue());       // this should return 0
