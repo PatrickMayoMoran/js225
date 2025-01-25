7f77cc9b59752853ec44485ac29beb2c4b7ac6d5
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-25 11:33:07 -0800

    Add own solution to factory function problem asking for default values that can be overridden using assign

---
+++ lesson5/createInvoices.js
@@ -0,0 +1,41 @@
+function createInvoice(services) {
+  // implement the factory function here
+  let invoice = {
+    internet: 5500,
+    phone: 3000,
+
+    total() {
+      return this.internet + this.phone;
+    }
+  }
+
+  return Object.assign(invoice, services);
+}
+
+function invoiceTotal(invoices) {
+  let total = 0;
+  let i;
+
+  for (i = 0; i < invoices.length; i += 1) {
+    total += invoices[i].total();
+  }
+
+  return total;
+}
+
+let invoices = [];
+invoices.push(createInvoice());
+invoices.push(createInvoice({
+  internet: 6500,
+}));
+
+invoices.push(createInvoice({
+  phone: 2000,
+}));
+
+invoices.push(createInvoice({
+  phone: 1000,
+  internet: 4500,
+}));
+
+console.log(invoiceTotal(invoices));             // => 31000
