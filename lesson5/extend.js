3bc69e1ebbc5346514cce55ac3b7de839e4e0f87
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-25 18:23:17 -0800

    Add solution to extend practice problem

---
+++ lesson5/extend.js
@@ -0,0 +1,37 @@
+function extend(destination, ...args) {
+  for (let i = 0; i < args.length; i++) {
+    let obj = args[i];
+    for (let prop in obj) {
+      destination[prop] = obj[prop];
+    }
+  }
+
+  return destination;
+}
+
+let foo = {
+  a: 0,
+  b: {
+    x: 1,
+    y: 2,
+  },
+};
+
+let joe = {
+  name: 'Joe'
+};
+
+let funcs = {
+  sayHello() {
+    console.log('Hello, ' + this.name);
+  },
+
+  sayGoodBye() {
+    console.log('Goodbye, ' + this.name);
+  },
+};
+
+let object = extend({}, foo, joe, funcs);
+
+console.log(object.b.x);          // => 1
+object.sayHello();                // => Hello, Joe
