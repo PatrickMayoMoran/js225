065cb2db6271d2a479290eb62c8eb200379289cd
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-27 12:39:33 -0800

    Add provided solution to ninjaNoAccess problem

--- lesson5/ninjaNoAccess.js
+++ lesson5/ninjaNoAccess.js
@@ -4,7 +4,6 @@ let ninjaA = (function() {
 })();
 
 // create a ninjaB object
-let ninjaB = Object.create(ninjaA);
-ninjaB.constructor = ninjaA.constructor;
+let ninjaB = new ninjaA.constructor()
 
 console.log(ninjaB.constructor === ninjaA.constructor);    // should log true
