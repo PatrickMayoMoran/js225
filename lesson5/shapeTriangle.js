6da91e89d15f008fdd4d41449cecaa18498b7cb6
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-27 13:58:35 -0800

    Add solution to shapeTriangle that includes resetting function prototype constructor to function

--- lesson5/shapeTriangle.js
+++ lesson5/shapeTriangle.js
@@ -16,6 +16,10 @@ Triangle.prototype.getPerimeter = function() {
   return this.a + this.b + this.c;
 }
 
+// When we manually set Triangle.prototype to shape, we lost connection to the
+// constructor. So we reset it here to reestablish the link!
+Triangle.prototype.constructor = Triangle
+
 let t = new Triangle(3, 4, 5);
 t.constructor;                 // Triangle(a, b, c)
 shape.isPrototypeOf(t);        // true
