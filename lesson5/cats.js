a4c8b8e51cca1460626a11115b4596add4a4bd7e
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-28 11:35:17 -0800

    Add method for renaming cat

--- lesson5/cats.js
+++ lesson5/cats.js
@@ -3,8 +3,18 @@
 class Cat {
   constructor(name="Kitty") {
     this.name = name;
+  }
+
+  greet() {
     console.log(`I'm a cat and my name is ${this.name}!`);
+  }
+
+  rename(newName) {
+    this.name = newName;
   }
 }
 
 let kitty = new Cat();
+kitty.greet();
+kitty.rename("Tiny");
+kitty.greet();
