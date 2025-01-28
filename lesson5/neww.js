1ad05fdcc6b7c9dfcb607c6b87fbeff068fa30de
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-27 20:20:58 -0800

    Show example of own implementation of new with constructor

---
+++ lesson5/neww.js
@@ -0,0 +1,19 @@
+function neww(constructor, args) {
+  // ..
+  let newObject = Object.create(constructor.prototype);
+  constructor.apply(newObject, args);
+  return newObject
+}
+
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+Person.prototype.greeting = function() {
+  console.log('Hello, ' + this.firstName + ' ' + this.lastName);
+};
+
+let john = neww(Person, ['John', 'Doe']);
+john.greeting();          // => Hello, John Doe
+console.log(john.constructor);         // Person(firstName, lastName) {...}
