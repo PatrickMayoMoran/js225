b6f4a685a4f36e82c5373b32f47ac675f19515a2
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-01-28 11:51:23 -0800

    Add skeleton for animals class inheritance problem

---
+++ lesson5/animals.js
@@ -0,0 +1,22 @@
+class Animal {
+  constructor(name, age, legs, species, status) {
+    this.name = name;
+    this.age = age;
+    this.legs = legs;
+    this.species = species;
+    this.status = status;
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old and ${this.status}.`;
+  }
+}
+
+class Cat extends Animal {
+}
+
+class Dog extends Animal {
+}
+
+let cat = new Cat("Pepe", 2, "happy");
+console.log(cat.introduce() === "Hello, my name is Pepe and I am 2 years old and happy. Meow meow!");
+// logs true
