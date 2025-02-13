2d0d36f0ba54c1330c63b50f99eea545731a060f
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-02-12 18:19:47 -0800

    Add example of context loss through a function as an argument

---
+++ review/contextLossThree.js
@@ -0,0 +1,11 @@
+// function as argument
+let tiny = {
+  name: 'Tiny',
+  eatTreats() {
+    ['goopie', 'crunchy', 'bunny'].forEach(function(treat) {
+      console.log(`${this.name} is happily eating ${treat}`);
+    });
+  },
+}
+
+tiny.eatTreats();
