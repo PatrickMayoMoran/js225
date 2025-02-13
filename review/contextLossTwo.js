ebc84a5e67a1de68bd2feba33c84b4b4fcdc25ca
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-02-12 17:44:20 -0800

    Add self = this or that = this lexical scoping solution to internal function context loss

--- review/contextLossTwo.js
+++ review/contextLossTwo.js
@@ -13,3 +13,18 @@ let tiny = {
 }
 
 tiny.meow() // wish for 'Tiny', get undefined
+
+// Solution 1 - lexical scoped variable of desired context
+let martin = {
+  name: "Martin",
+  meow() {
+    let self = this // lock in self as holding object
+    function purr() {
+      console.log(`I am a happy cat purring, my name is ${self.name}`);
+    };
+
+    purr();
+  },
+};
+
+martin.meow()
