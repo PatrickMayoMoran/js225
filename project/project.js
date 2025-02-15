d2370d659a35e9af8878c02bc64a472bdfe3e2e4
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-02-15 14:10:42 -0800

    Add implementation for range

--- project/project.js
+++ project/project.js
@@ -12,10 +12,23 @@
       without(...args) {
         return element.filter(e => !args.includes(e));
       },
+
     };
 
     return u;
   };
 
   window._ = _;
+
+  _.range = function(...args) {
+    let start, end;
+    start = args.length === 1 ? 0 : args[0];
+    end = args.length === 1 ? args[0] : args[1];
+    let newRange = [];
+    for (let i = start; i < end; i++) {
+      newRange.push(i);
+    };
+    return newRange;
+    };
+
 })();
