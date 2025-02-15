4e946d05c4d53896e8b82e9fa90dafd49b750713
Patrick Moran <patrick.mayo.moran@gmail.com>
2025-02-15 12:56:56 -0800

    Add example showing how using constructor function without new in strict mode will raise an error

---
+++ review/noNewError.js
@@ -0,0 +1,12 @@
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.type = "triangle";
+  }
+}
+
+
+let t = Triangle(1, 2, 3);
+console.log(t.a); // 1
